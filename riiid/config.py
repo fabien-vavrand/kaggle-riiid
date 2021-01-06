f5aa08fb209156b310277e32a58a88188d68c4e3
rfbr <fbr.romain@gmail.com>
2021-01-06 21:08:13 +0000

    cfg

--- riiid/config.py
+++ riiid/config.py
@@ -1,14 +1,14 @@
 import os
 import pickle
+
 import numpy as np
 
-
-PATH = r'C:\Users\chass\Kaggle\riiid'
-INPUT_PATH = os.path.join(PATH, 'data')
-MODELS_PATH = os.path.join(PATH, 'models')
-SUBMIT_PATH = os.path.join(PATH, 'submit')
-TEST_PATH = os.path.join(PATH, 'tests')
-TUNE_PATH = os.path.join(PATH, 'tuning')
+PATH = r"/home/romain/kaggle-riiid"
+INPUT_PATH = os.path.join(PATH, "data")
+MODELS_PATH = os.path.join(PATH, "models")
+SUBMIT_PATH = os.path.join(PATH, "submit")
+TEST_PATH = os.path.join(PATH, "tests")
+TUNE_PATH = os.path.join(PATH, "tuning")
 
 
 FLOAT_DTYPE = np.float32
@@ -16,81 +16,60 @@ ACTIVATE_CACHE = True
 
 
 PARAMS = {
-    'n_fold': 5,
-    'question_embedding': {
-        'n_clusters': 67,
-        'embedding_size': 56,
-        'window': 5,
-        'min_count': 1,
-        'sg': 0,
-        'iter': 15,
-        'workers': 1
+    "n_fold": 5,
+    "question_embedding": {
+        "n_clusters": 67,
+        "embedding_size": 56,
+        "window": 5,
+        "min_count": 1,
+        "sg": 0,
+        "iter": 15,
+        "workers": 1,
     },
-    'answers_embedding': {
-        'n_fold': 5,
-        'embedding_size': 68,
-        'window': 3,
-        'min_count': 5,
-        'sg': 1,
-        'iter': 20,
-        'workers': 1
+    "answers_embedding": {
+        "n_fold": 5,
+        "embedding_size": 68,
+        "window": 3,
+        "min_count": 5,
+        "sg": 1,
+        "iter": 20,
+        "workers": 1,
     },
-    'answer_encoder': {
-        'smoothing_min': 5,
-        'smoothing_value': 1,
+    "answer_encoder": {
+        "smoothing_min": 5,
+        "smoothing_value": 1,
     },
-    'score_encoder': {
-        'smoothing_min': 5,
-        'smoothing_value': 1,
-        'noise': 0.001
+    "score_encoder": {"smoothing_min": 5, "smoothing_value": 1, "noise": 0.001},
+    "score_encoder_2": {"smoothing_min": 20, "smoothing_value": 5, "noise": 0.002},
+    "user_score_encoder": {"smoothing_min": 5, "smoothing_value": 2},
+    "user_content_score_encoder": {"smoothing_min": None, "smoothing_value": 0.5},
+    "user_rolling_score_encoder": {"smoothing_min": 2.5, "smoothing_value": 1, "rolling": 20},
+    "user_weighted_score_encoder": {
+        "smoothing_min": 2,
+        "smoothing_value": 1,
     },
-    'score_encoder_2': {
-        'smoothing_min': 20,
-        'smoothing_value': 5,
-        'noise': 0.002
+    "lgbm_params_raw": {
+        "objective": "binary",
+        "boosting_type": "gbdt",
+        "metric": "auc",
+        "learning_rate": 0.1,
+        "verbosity": -1,
     },
-    'user_score_encoder': {
-        'smoothing_min': 5,
-        'smoothing_value': 2
+    "lgbm_params": {
+        "objective": "binary",
+        "boosting_type": "gbdt",
+        "min_data_in_leaf": 800,
+        "num_leaves": 1100,
+        "bagging_fraction": 0.99,
+        "feature_fraction": 0.7,
+        "metric": "auc",
+        "bagging_freq": 5,
+        "learning_rate": 0.02,
     },
-    'user_content_score_encoder': {
-        'smoothing_min': None,
-        'smoothing_value': 0.5
+    "mlp_params": {
+        "layers": [1024, 1024, 1024, 1024, 512, 256, 128, 16],
+        "dropout": 0.2,
+        "epochs": 100,
+        "batch_size": 256,
     },
-    'user_rolling_score_encoder': {
-        'smoothing_min': 2.5,
-        'smoothing_value': 1,
-        'rolling': 20
-    },
-    'user_weighted_score_encoder': {
-        'smoothing_min': 2,
-        'smoothing_value': 1,
-    },
-    'lgbm_params_raw': {
-        'objective': 'binary',
-        'boosting_type': 'gbdt',
-        'metric': 'auc',
-        'learning_rate': 0.1,
-        'verbosity': -1
-    },
-    'lgbm_params': {
-        'objective': 'binary',
-        'boosting_type': 'gbdt',
-        'metric': 'auc',
-        'learning_rate': 0.01,
-        'feature_fraction': 0.7,
-        'bagging_fraction': 0.9,
-        'bagging_freq': 5,
-        'num_leaves': 500,
-        # 'min_child_samples': 20,
-        # 'lambda_l1': 1.7e-3,
-        # 'lambda_l2': 4.3e-7,
-        'verbosity': -1
-    },
-    'mlp_params': {
-        'layers': [1024, 1024, 1024, 1024, 512, 256, 128, 16],
-        'dropout': 0.2,
-        'epochs': 100,
-        'batch_size': 256
-    }
 }
